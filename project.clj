0850b590e23809038fb32868b6425d216bc6936e
Neale Swinnerton <neale@isismanor.com>
2014-07-25 11:17:50 +0100

    Add Cascalog.

--- project.clj
+++ project.clj
@@ -3,4 +3,9 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]])
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [cascalog "2.0.0"]]
+
+  :profiles { :dev {:dependencies [[org.apache.hadoop/hadoop-core "1.1.2"]]}}
+
+  :jvm-opts ["-Xms768m" "-Xmx768m"])
