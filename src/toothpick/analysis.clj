2c3d5e5d3a3e674c41bedb7bc3ea2828094e2d6b
Neale Swinnerton <neale@isismanor.com>
2014-07-28 14:29:57 +0100

    Further cascalog.

--- src/toothpick/analysis.clj
+++ src/toothpick/analysis.clj
@@ -2,8 +2,9 @@
   (:require [cascalog.api :refer :all]
             [cascalog.more-taps :refer (hfs-delimited)]
             [clojure.string :as s]
-            ))
+            [clj-time.core :as tf]))
 
+(def date-time-formatter (tf/formatter "dd-MM-yyyy hh:mm"))
 
 (defmapfn split [line]
   "reads in a line of string and splits it by regex"
@@ -13,6 +14,9 @@
   "trim open whitespaces and lower case"
   ((comp s/trim s/lower-case) s))
 
+;; 01-01-2014,00:15
+(defn parse-date-time [date time]
+  (tf/parse date-time-formatter (str date " " time)))
 
 ;; Date,time,"Modelled Wind Direction",status/unit,"Modelled Wind Speed",status/unit,"Modelled Temperature",status/unit,"Nitric oxide",status/unit,"Nitrogen dioxide",status/unit,"Nitrogen oxides as nitrogen dioxide",status/unit,"Non-volatile PM2.5 (Hourly measured)",status/unit,"PM2.5 particulate matter (Hourly measured)",status/unit,"Sulphur dioxide",status/unit,"Volatile PM2.5 (Hourly measured)",status/unit
 
@@ -20,9 +24,9 @@
   (= n (count line )))
 
 (defn borough-gen [raw-in]
-  (<- [?date ?time ?no2]
+  (<- [?date-time ?no2]
       (raw-in :>
-              ?date ?time
+              ?date-dirty ?time-dirty
               ?wind-direction ?wind-direction-unit
               ?wind-speed ?wind-speed-unit
               ?temperature ?temperature-unit
@@ -33,10 +37,11 @@
               ?pm25-particulate-matter-hourly ?pm25-particulate-matter-hourly-unit
               ?so2 ?so2-unit
               ?v-pm25-hourly ?v-pm25-hourly-unit)
-
+      (parse-date-time ?date-dirty ?time-dirty :> ?date-time)
       (:trap (hfs-textline "output/trap" :sinkmode :update))))
 
 (defn go []
   (let [borough (hfs-delimited "/home/neale/workspace/toothpick/datasets/uk-air/lb-bexley-2.csv")
+        output (hfs-delimited "output/borough")
         trap (hfs-delimited "output/trap")]
-    (borough-gen borough)))
+    (?- output (borough-gen borough))))
