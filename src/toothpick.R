5cd58c26500cfac6977d65fec7aa3d2f24d80b8a
Francine Bennett <talktofran@googlemail.com>
2014-07-30 19:06:12 +0100

    corrections for boundary codes in St Albans

--- src/toothpick.R
+++ src/toothpick.R
@@ -1,3 +1,10 @@
+## TOOTHPICK SCRIPT
+
+## - ingests and rolls up various health datasets to borough level
+## - ranks each metric per borough (normalised rank centred on zero)
+## - creates overall rank by taking means of individual metric ranks
+## - outputs csv with raw metrics, metric-level ranks, and overall rank, for each borough
+
 # Location where copy of raw data is stored
 setwd("/Users/francinebennett/Dropbox/datasets/") 
 
@@ -17,6 +24,8 @@ cycling.frequency<-read.xls("local-area-walking-and-cycling/cw0111.xls",skip=7)
 cycling.frequency$LA.name<-paste(cycling.frequency$X,cycling.frequency$X.1,sep="")
 cycling.frequency<-subset(cycling.frequency,!is.na("X1.x.per.week") & LA.name!="") 
 cycling.frequency<-cycling.frequency[,c("LA.code","LA.name","X1.x.per.week")] ## Use pct of people who cycle at least once per week
+cycling.frequency$LA.code<-gsub("E07000100","E07000240",cycling.frequency$LA.code) # Update St Albans codes for post boundary changes
+cycling.frequency$LA.code<-gsub("E07000104","E07000241",cycling.frequency$LA.code)
 names(cycling.frequency)[3]<-"cycling_weekly"
 cycling.frequency$cycling.rank<-
   scale(rank(cycling.frequency$cycling_weekly),
@@ -27,6 +36,8 @@ walking.frequency<-read.xls("local-area-walking-and-cycling/cw0121.xls",skip=7)
 walking.frequency$LA.name<-paste(walking.frequency$X,walking.frequency$X.1,sep="")
 walking.frequency<-subset(walking.frequency,!is.na("X3.x.per.week") & LA.name!="") 
 walking.frequency<-walking.frequency[,c("LA.code","LA.name","X3.x.per.week")] ## Use pct of people who walk at least 3 times per week
+walking.frequency$LA.code<-gsub("E07000100","E07000240",walking.frequency$LA.code) # Update St Albans codes for post boundary changes
+walking.frequency$LA.code<-gsub("E07000104","E07000241",walking.frequency$LA.code)
 names(walking.frequency)[3]<-"walking_thriceweekly"
 walking.frequency$walking.rank<-scale(rank(walking.frequency$walking_thriceweekly),center=TRUE,scale=FALSE)
 
@@ -49,11 +60,10 @@ patient.experience<-read.csv("GPOutcomes/patient_experience.csv",header=FALSE)
 patient.experience<-subset(patient.experience,V3 %in% c("Able to see a doctor fairly quickly - total responses","Able to see a doctor fairly quickly - Yes"))
 patient.experience<-patient.experience[,c("V1","V3","V4")]
 patient.experience<-cast(patient.experience,V1~V3)
-#patient.experience$pct_quick<-patient.experience[,3]/patient.experience[,2]
 practice.locations<-read.csv("practice-to-borough.csv",sep="\t",header=FALSE)
 patient.experience<-merge(patient.experience,practice.locations,all.x=TRUE)
 names(patient.experience)<-c("practice_code","question_responses","positive_responses","LA.code")
-borough.gp.experience<-ddply(patient.experience, .(LA.code), summarize, question_responses = sum(question_responses), positive_responses = sum(positive_responses,na.rm=TRUE))
+borough.gp.experience<-ddply(patient.experience, .(LA.code), summarize, question_responses = sum(question_responses,na.rm=TRUE), positive_responses = sum(positive_responses,na.rm=TRUE))
 borough.gp.experience$pct_canseegp<-borough.gp.experience$positive_responses/borough.gp.experience$question_responses
 borough.gp.experience$gp.rank<-scale(rank(borough.gp.experience$pct_canseegp),center=TRUE,scale=FALSE)
 borough.gp.experience<-borough.gp.experience[,c("LA.code","pct_canseegp","gp.rank")]
@@ -80,6 +90,8 @@ dentists.borough<-ddply(dentists, .(LA.code), summarize, dental_practitioners = 
 # Normalise count of dentists by population estimate from ONS at http://www.neighbourhood.statistics.gov.uk/dissemination/instanceSelection.do?JSAllowed=true&Function=&%24ph=60_61_60_61&CurrentPageId=61&step=2&datasetFamilyId=1813&instanceSelection=134023&Next.x=9&Next.y=5
 population<-read.csv("K30A0312_2725_2011SOA_LA.CSV",skip=2,stringsAsFactors=FALSE)
 population<-population[,c("X","All.Persons..All.Ages")]
+population$X<-gsub("E07000100","E07000240",population$X) # Update St Albans codes for post boundary changes
+population$X<-gsub("E07000104","E07000241",population$X)
 population$All.Persons..All.Ages<-as.numeric(population$All.Persons..All.Ages)
 dentists.borough<-merge(dentists.borough,population,by.x="LA.code",by.y="X",all.x=TRUE)
 dentists.borough$dentists_per_thousand<-dentists.borough$dental_practitioners/(dentists.borough$All.Persons..All.Ages/1000)
@@ -112,5 +124,5 @@ borough.healthscore$overall.rank<-
 
 borough.healthscore<-subset(borough.healthscore,!is.na(LA.name) & !is.na(LA.code))
 borough.healthscore<-borough.healthscore[order(borough.healthscore$overall.rank,decreasing=TRUE),]
-names(borough.healthscore)<-gsub(".","_",names(borough.healthscore))
+names(borough.healthscore)<-gsub("\\.","_",names(borough.healthscore))
 write.csv(borough.healthscore,"borough_scores.csv",quote=FALSE,row.names=FALSE)
