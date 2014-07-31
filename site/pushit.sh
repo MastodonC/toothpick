dc7c1dcc69200e2fc1ff90c4427de4b0b62a7f4f
Neale Swinnerton <neale@isismanor.com>
2014-07-31 11:52:33 +0100

    Site has moved to OHDP repo.

--- site/pushit.sh
+++
@@ -1,3 +0,0 @@
-#! /bin/bash
-
-aws --profile mastodonc s3 sync . s3://openhealthdata.cdehub.org/ --acl public-read --exclude '.git*' --exclude '*.DS_Store' --exclude '*~' --exclude pushit.sh --delete
