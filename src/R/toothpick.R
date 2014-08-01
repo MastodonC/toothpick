50c6df16cce41f5c5f8e1cf3d2a929e880b5e7b4
Francine Bennett <talktofran@googlemail.com>
2014-08-01 14:01:05 +0100

    updated to add count of dental surgeries, and to remove county councils from the file

--- src/R/toothpick.R
+++ src/R/toothpick.R
@@ -17,6 +17,15 @@ require("foreign")
 require("plyr")
 require("lubridate")
 
+## POPULATION DATA
+# populations per local authority from ONS at http://www.neighbourhood.statistics.gov.uk/dissemination/instanceSelection.do?JSAllowed=true&Function=&%24ph=60_61_60_61&CurrentPageId=61&step=2&datasetFamilyId=1813&instanceSelection=134023&Next.x=9&Next.y=5
+# Used to normalise statistics per head, whre needed
+population<-read.csv("K30A0312_2725_2011SOA_LA.CSV",skip=2,stringsAsFactors=FALSE)
+population<-population[,c("X","All.Persons..All.Ages")]
+population$X<-gsub("E07000100","E07000240",population$X) # Update St Albans codes for post boundary changes
+population$X<-gsub("E07000104","E07000241",population$X)
+population$All.Persons..All.Ages<-as.numeric(population$All.Persons..All.Ages)
+
 ## CYCLING AND WALKING DATA
 ## Use cycling and walking frequency data from DfT transport mode survey at https://www.gov.uk/government/statistics/local-area-walking-and-cycling-in-england-2012-to-2013
 
@@ -53,9 +62,10 @@ mene.borough<-subset(mene.borough,LA.name!="0")
 mene.borough$greenspace.rank<-scale(rank(mene.borough$weekly_greenspace_visits),center=TRUE,scale=FALSE)
 
 ## GP SURGERY AND QUALITY DATA
-## Patient experience of being able to see a doctor fairly quickly P01146
+## Patient experience of being able to see a doctor fairly quickly - indicator P01146
 ## Filtered from original file using a terminal command 
 ## grep "P01146" results.csv > patient_experience.csv
+## Calculate count of practices and percentage of patients who can see a doctor fairly quickly per borough
 patient.experience<-read.csv("GPOutcomes/patient_experience.csv",header=FALSE)
 patient.experience<-subset(patient.experience,V3 %in% c("Able to see a doctor fairly quickly - total responses","Able to see a doctor fairly quickly - Yes"))
 patient.experience<-patient.experience[,c("V1","V3","V4")]
@@ -63,17 +73,18 @@ patient.experience<-cast(patient.experience,V1~V3)
 practice.locations<-read.csv("practice-to-borough.csv",sep="\t",header=FALSE)
 patient.experience<-merge(patient.experience,practice.locations,all.x=TRUE)
 names(patient.experience)<-c("practice_code","question_responses","positive_responses","LA.code")
-borough.gp.experience<-ddply(patient.experience, .(LA.code), summarize, question_responses = sum(question_responses,na.rm=TRUE), positive_responses = sum(positive_responses,na.rm=TRUE))
+borough.gp.experience<-ddply(patient.experience, 
+                             .(LA.code), 
+                             summarize, 
+                             question_responses = sum(question_responses,na.rm=TRUE), 
+                             positive_responses = sum(positive_responses,na.rm=TRUE),
+                             number_gp_practices = length(practice_code))
 borough.gp.experience$pct_canseegp<-borough.gp.experience$positive_responses/borough.gp.experience$question_responses
 borough.gp.experience$gp.rank<-scale(rank(borough.gp.experience$pct_canseegp),center=TRUE,scale=FALSE)
-borough.gp.experience<-borough.gp.experience[,c("LA.code","pct_canseegp","gp.rank")]
+borough.gp.experience<-borough.gp.experience[,c("LA.code","number_gp_practices","pct_canseegp","gp.rank")]
 
 ## HOSPITAL FRIENDS AND FAMILY DATA
 hospital.ae<-read.csv("Friends and Family/FFT_AE_csv4.csv",skip=2)
-hospital.ip<-read.csv("Friends and Family/FFT_IP_csv4.csv",skip=2)
-hospital.maternity<-read.csv("Friends and Family/FFT_Mat_csv3.csv",skip=2)
-
-# Filter to just the site codes and test scores. Using only A&E scores as other response rates are very los
 hospital.ae<-hospital.ae[,c("Site.Code","Friends.and.Family.Test.Score")]
 hospital.locations<-read.csv("hospital-locations.csv",sep="\t",header=FALSE)
 hospital.ae<-merge(hospital.ae,hospital.locations,by.x="Site.Code",by.y="V1",all.x=TRUE)
@@ -87,12 +98,6 @@ dentist.locations<-read.csv("dentists-to-borough.csv",sep="\t",header=FALSE)
 dentists<-merge(dentists,dentist.locations,by.x="id",by.y="V1",all.x=TRUE)
 names(dentists)<-c("dentist_id","nhs_fees","postcode","dental_practitioners","review_score","LA.code")
 dentists.borough<-ddply(dentists, .(LA.code), summarize, dental_practitioners = sum(dental_practitioners))
-# Normalise count of dentists by population estimate from ONS at http://www.neighbourhood.statistics.gov.uk/dissemination/instanceSelection.do?JSAllowed=true&Function=&%24ph=60_61_60_61&CurrentPageId=61&step=2&datasetFamilyId=1813&instanceSelection=134023&Next.x=9&Next.y=5
-population<-read.csv("K30A0312_2725_2011SOA_LA.CSV",skip=2,stringsAsFactors=FALSE)
-population<-population[,c("X","All.Persons..All.Ages")]
-population$X<-gsub("E07000100","E07000240",population$X) # Update St Albans codes for post boundary changes
-population$X<-gsub("E07000104","E07000241",population$X)
-population$All.Persons..All.Ages<-as.numeric(population$All.Persons..All.Ages)
 dentists.borough<-merge(dentists.borough,population,by.x="LA.code",by.y="X",all.x=TRUE)
 dentists.borough$dentists_per_thousand<-dentists.borough$dental_practitioners/(dentists.borough$All.Persons..All.Ages/1000)
 dentists.borough<-dentists.borough[,c("LA.code","dentists_per_thousand")]
@@ -124,5 +129,6 @@ borough.healthscore$overall.rank<-
 
 borough.healthscore<-subset(borough.healthscore,!is.na(LA.name) & !is.na(LA.code))
 borough.healthscore<-borough.healthscore[order(borough.healthscore$overall.rank,decreasing=TRUE),]
+borough.healthscore<-borough.healthscore[-grep(pattern = "^E1",borough.healthscore$LA_code),] # Remove county councils, where codes begin with E1
 names(borough.healthscore)<-gsub("\\.","_",names(borough.healthscore))
 write.csv(borough.healthscore,"borough_scores.csv",quote=FALSE,row.names=FALSE)
